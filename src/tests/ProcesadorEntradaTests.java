fbbe085a3d9175a4eaf204d57015a18e3a329c5e
catalina <catapf493@yahoo.com>
2018-03-27 12:40:39 -0500

    Se organizan las pruebas automaticas del ProcesadorEntrada

--- src/tests/ProcesadorEntradaTests.java
+++ src/tests/ProcesadorEntradaTests.java
@@ -3,213 +3,148 @@ package tests;
 import main.ProcesadorEntrada;
 import static org.junit.Assert.*;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class ProcesadorEntradaTests {
 
 	@Test
-	public void testProcesarEspacioDigitos_ValorValido() {
-		String entrada;
-		int esperado;
-		int dado;
-		
-		entrada = "0";
-		esperado = 0;
-		dado = ProcesadorEntrada.procesarEspacioDigitos(entrada);
-		assertEquals(esperado, dado);
-		
-		entrada = "5";
-		esperado = 5;
-		dado = ProcesadorEntrada.procesarEspacioDigitos(entrada);
-		assertEquals(esperado, dado);
+	public void procesarEspacioDigitos_ValorMinimo() {		
+		assertEquals(0, ProcesadorEntrada.procesarEspacioDigitos("0"));
+	}
+
+	@Test
+	public void procesarEspacioDigitos_ValorMaximo() {		
+		assertEquals(5, ProcesadorEntrada.procesarEspacioDigitos("5"));
 	}	
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarEspacioDigitos_ValorMenorAlRango() {
-		String entrada;
-		
-		entrada = "-1";
-		ProcesadorEntrada.procesarEspacioDigitos(entrada);
+
+	@Test
+	public void procesarEspacioDigitos_ValorMedio() {		
+		assertEquals(3, ProcesadorEntrada.procesarEspacioDigitos("3"));
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarEspacioDigitos_ValorMayorAlRango() {
-		String entrada;
-		
-		entrada = "6";
-		ProcesadorEntrada.procesarEspacioDigitos(entrada);
+	public void procesarEspacioDigitos_ValorMenorAlRango() {
+		ProcesadorEntrada.procesarEspacioDigitos("-1");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarEspacioDigitos_EntradaVacia() {
-		String entrada;
-		
-		entrada = "";
-		ProcesadorEntrada.procesarEspacioDigitos(entrada);
+	public void procesarEspacioDigitos_ValorMayorAlRango() {
+		ProcesadorEntrada.procesarEspacioDigitos("6");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarEspacioDigitos_EntradaNoValida() {
-		String entrada;
-		
-		entrada = "*";
-		ProcesadorEntrada.procesarEspacioDigitos(entrada);
+	public void procesarEspacioDigitos_EntradaVacia() {
+		ProcesadorEntrada.procesarEspacioDigitos("");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void procesarEspacioDigitos_EntradaNoValida() {
+		ProcesadorEntrada.procesarEspacioDigitos("*");
 	}
 
 
 	@Test
-	public void testFinComandos_EsUltimoComando() {
-		String comando;
-		
-		comando = "0,0";		
-		assertTrue(ProcesadorEntrada.finComandos(comando));
+	public void finComandos_EsUltimoComando() {
+		assertTrue(ProcesadorEntrada.finComandos("0,0"));
 	}
 	
 	@Test
-	public void testFinComandos_NoEsUltimoComando() {
-		String comando;
-		
-		comando = "";		
-		assertFalse(ProcesadorEntrada.finComandos(comando));
-		
-		comando = "0,1";
-		assertFalse(ProcesadorEntrada.finComandos(comando));
-		
-		comando = "t,0";
-		assertFalse(ProcesadorEntrada.finComandos(comando));
-		
-		comando = "2,1";
-		assertFalse(ProcesadorEntrada.finComandos(comando));
+	public void finComandos_ComandoVacio() {
+		assertFalse(ProcesadorEntrada.finComandos(""));
+	}
+	
+	@Test
+	public void finComandos_ComandoValidoNoUltimo() {
+		assertFalse(ProcesadorEntrada.finComandos("0,1"));
+	}
+	
+	@Test
+	public void finComandos_ComandoConCaracterNoValido() {
+		assertFalse(ProcesadorEntrada.finComandos("t,0"));
 	}
 
 	@Test
-	public void testProcesarComando_ComandoValido() {
-		String comando;
-		String[] parametros;
-		String parametro0, parametro1;
-		
-		comando = "3,5";
-		parametro0 = "3";
-		parametro1 = "5";
-		parametros = ProcesadorEntrada.procesarComando(comando);
+	public void procesarComando_ComandoValido() {
+		String[] parametros = ProcesadorEntrada.procesarComando("3,5");
 		
 		assertEquals(2, parametros.length);
-		assertEquals(parametro0, parametros[0]);
-		assertEquals(parametro1, parametros[1]);
+		assertEquals("3", parametros[0]);
+		assertEquals("5", parametros[1]);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarComando_ComandoMasLargo() {
-		String comando;
-		
-		comando = "3,5,8";
-		ProcesadorEntrada.procesarComando(comando);		
+	public void procesarComando_ComandoMasLargo() {
+		ProcesadorEntrada.procesarComando("3,5,8");		
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarComando_ComandoSinSeparador() {
-		String comando;
-		
-		comando = "3 8";
-		ProcesadorEntrada.procesarComando(comando);	
+	public void procesarComando_ComandoSinSeparador() {
+		ProcesadorEntrada.procesarComando("3 8");	
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarComando_ComandoMasCorto() {
-		String comando;
-		
-		comando = "3,";
-		ProcesadorEntrada.procesarComando(comando);	
+	public void procesarComando_ComandoMasCorto() {
+		ProcesadorEntrada.procesarComando("3,");	
 	}
 
 	@Test
-	public void testProcesarSize_ValorValido() {
-		String entrada;
-		int size;
-		
-		entrada = "1";
-		size = 1;
-		
-		assertEquals(size, ProcesadorEntrada.procesarSize(entrada));
-		
-		entrada = "10";
-		size = 10;
-		
-		assertEquals(size, ProcesadorEntrada.procesarSize(entrada));
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarSize_ValorMayorAlRango() {
-		String entrada;
-		
-		entrada = "11";
-		ProcesadorEntrada.procesarSize(entrada);
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarSize_ValorMenorAlRango() {
-		String entrada;
-		
-		entrada = "0";
-		ProcesadorEntrada.procesarSize(entrada);
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarSize_EntradaNoVacia() {
-		String entrada;
-		
-		entrada = "";
-		ProcesadorEntrada.procesarSize(entrada);
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarSize_EntradaNoValida() {
-		String entrada;
-		
-		entrada = "*";
-		ProcesadorEntrada.procesarSize(entrada);
+	public void procesarSize_ValorMinimo() {		
+		assertEquals(1, ProcesadorEntrada.procesarSize("1"));
 	}
 
 	@Test
-	public void testProcesarNumero_ValorValido() {
-		String entrada;
-		int numero;
-		
-		entrada = "12345678";
-		numero = 12345678;
-				
-		assertEquals(numero, ProcesadorEntrada.procesarNumero(entrada));
-		
-		entrada = "0";
-		numero = 0;
-		
-		assertEquals(numero, ProcesadorEntrada.procesarNumero(entrada));
+	public void procesarSize_ValorMaximo() {
+		assertEquals(10, ProcesadorEntrada.procesarSize("10"));
+	}
+
+	@Test
+	public void procesarSize_ValorMedio() {
+		assertEquals(4, ProcesadorEntrada.procesarSize("4"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarNumero_NumeroNegativo() {
-		String entrada;
-		
-		entrada = "-12345678";
-		ProcesadorEntrada.procesarNumero(entrada);		
+	public void procesarSize_ValorMayorAlRango() {
+		ProcesadorEntrada.procesarSize("11");
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarNumero_EntradaVacia() {
-		String entrada;
-		
-		entrada = "";
-		ProcesadorEntrada.procesarNumero(entrada);		
+	public void procesarSize_ValorMenorAlRango() {
+		ProcesadorEntrada.procesarSize("0");
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testProcesarNumero_EntradaNoValida() {
-		String entrada;
-		
-		entrada = "123*5678";
-		ProcesadorEntrada.procesarNumero(entrada);		
+	public void procesarSize_EntradaNoVacia() {
+		ProcesadorEntrada.procesarSize("");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void procesarSize_EntradaNoValida() {
+		ProcesadorEntrada.procesarSize("*");
+	}
+
+	@Test
+	public void procesarNumero_ValorValido() {
+		assertEquals(12345678, ProcesadorEntrada.procesarNumero("12345678"));
+	}
+
+	@Test
+	public void procesarNumero_ValorMinimo() {
+		assertEquals(0, ProcesadorEntrada.procesarNumero("0"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void procesarNumero_ValorNegativo() {
+		ProcesadorEntrada.procesarNumero("-12345678");		
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void procesarNumero_EntradaVacia() {
+		ProcesadorEntrada.procesarNumero("");		
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void procesarNumero_EntradaNoValida() {
+		ProcesadorEntrada.procesarNumero("123*5678");		
 	}
 
 }
